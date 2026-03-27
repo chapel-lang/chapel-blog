f66d7448e073953eee4033208684f4d1c50b7d5c
Danila Fedorin <daniel.fedorin@hpe.com>
2026-03-27 14:22:05 -0700

    Add Playwright to CI config.
    Signed-off-by: Danila Fedorin <daniel.fedorin@hpe.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,9 +46,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'python3 -m http.server 1313 --directory ./public',
+    url: 'http://localhost:1313',
+    reuseExistingServer: !process.env.CI,
+  },
 });
